1dcd626ca3821220228b8314ca17e64d09ef130d
IBON International <leopura@ITs-MacBook-Air.local>
2026-04-21 23:35:45 +0800

    Export EmployeeAttachmentWithUploader type from services index

--- src/hooks/useEmployeeAttachments.ts
+++ src/hooks/useEmployeeAttachments.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { employeeAttachmentService, type EmployeeAttachmentWithUploader } from '@/services/employeeAttachment.service'
+import { employeeAttachmentService, type EmployeeAttachmentWithUploader } from '@/services'
 import { toast } from 'sonner'
 
 const attachmentKeys = {
